48061cc63937b905d79cd63c12fbc9e0fae00fb2
okaymahdi <mahdimunshee@gmail.com>
2024-02-23 00:14:45 +0600

    updated 03-more-map.js

--- MODULE_31_ADVANED_MORE_ABOUT_ES6/03-more-map.js
+++ MODULE_31_ADVANED_MORE_ABOUT_ES6/03-more-map.js
@@ -2,20 +2,20 @@ const numbers = [12, 10, 8, 15, 7];
 
 /** multiple any numbers by 2 */
 const doubleOne = numbers.map(num => num * 2);
-// console.log(doubleOne);
+console.log(doubleOne);
 
 /** added 5 with any numbers */
 const fiveAdded = numbers.map(num => num + 5);
-// console.log(fiveAdded);
+console.log(fiveAdded);
 
 /** divided any numbers by 2 */
 const halves = numbers.map(num => num / 2);
-// console.log(halves);
+console.log(halves);
 
 /** length of array */
 const friends = ['Abid', 'Afia', 'Mahid', 'Baba', 'Ammu'];
 const length = friends.map(friend => friend.length);
-// console.log(length);
+console.log(length);
 
 /** first letter show */
 const firstLetter = friends.map(friend => friend[0]);
